a79b8aea05f2bc280afe75231f73411ac8383938
Kangyun Wang <wky@155.155.028.145.hva.nl>
2018-06-06 17:54:24 +0200

    Set base for selection screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-    <h1>{{title}}</h1>
+	<main>
+    	<h1>{{ title }}</h1>
+	 	<router-link to="/selection">Selection</router-link>
+	</main>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            title: 'Home page'
-        };
-    }
-};
+	export default {
+		data() {
+			return {
+					title: 'Home page'
+			};
+		}
+	};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/views/Selection.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<h2>Header and back</h2>
+		<section>List of first half</section>
+		<section>List of last half</section>
+		<footer>
+			<section>selection</section>
+			<button @click="test">confirm</button>
+		</footer>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['backTo'],
+		data() {
+			return {}
+		},
+		methods: {
+			test() {
+				console.log('Got clicked');
+			}
+		},
+		created() {}
+	}
+</script>
+
+<style lang="scss" scoped>
+</style>
