c8621c912b881eb8b7ce03ba5577d9ac04d13bac
VincentKempers <me@vncn.tk>
2018-06-08 10:48:29 +0200

    change the alt text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,13 +11,13 @@
 			</div>
 			<div class="pictures-in-twos">
 				<div class='text-image'>
-				<img src="https://upload.wikimedia.org/wikipedia/commons/b/b4/Vincent_Willem_van_Gogh_128.jpg" alt="people sitting in a restaurant painting">
+				<img src="https://upload.wikimedia.org/wikipedia/commons/b/b4/Vincent_Willem_van_Gogh_128.jpg" alt="sunflower painting van gogh">
 				<div class="text-block">
 					<h4>Open tour</h4>
 				</div>
 			</div>
 				<div class='text-image'>
-				<img src="https://www.vangoghgallery.com/catalog/image/0320/Self-Portrait.jpg" alt="people sitting in a restaurant painting">
+				<img src="https://www.vangoghgallery.com/catalog/image/0320/Self-Portrait.jpg" alt="Self portrait of van Gogh">
 				<div class="text-block">
 					<h4>Highlight tour</h4>
 				</div>
