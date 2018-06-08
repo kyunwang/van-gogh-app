1df0d0afb8bab655a23c0e36c603d85c09a6bf91
VincentKempers <me@vncn.tk>
2018-06-08 12:37:41 +0200

    add tourmap vue

---
+++ src/views/Tourmap.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <h1>{{title}}</h1>
+        <div class="map">
+            <img src="/assets/floor1.svg" alt="floor 1">
+        </div>
+        <button>Go to next floor</button>
+    </main>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: `Floor 1`,
+		};
+	},
+};
+</script>
+
+<style>
+h1 {
+	color: white;
+}
+</style>
