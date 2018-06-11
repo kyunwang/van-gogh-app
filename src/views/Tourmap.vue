dc07e47ab831df70bfbc8bfc0ff7b4673559d712
VincentKempers <me@vncn.tk>
2018-06-11 10:40:17 +0200

    add button animation and added the theme separation of friday

--- src/views/Tourmap.vue
+++ src/views/Tourmap.vue
@@ -3,8 +3,9 @@
         <h1>{{title}}</h1>
         <div class="map">
             <img src="/assets/floor1.svg" alt="floor 1">
+			<a href="#"><div class="circle"> 60 </div></a>
         </div>
-        <button>Go to next floor</button>
+        <button>Skip floor</button>
     </main>
 </template>
 
@@ -18,8 +19,32 @@ export default {
 };
 </script>
 
-<style>
-h1 {
-	color: white;
+<style lang="scss" scoped>
+main {
+	margin: 0 0.8em;
+
+	h1 {
+		color: white;
+	}
+	.map {
+		width: 100%;
+		img {
+			width: 22em;
+		}
+	}
+	button {
+		position: fixed;
+		right: 4;
+		bottom: -30px;
+		background-color: black;
+		color: white;
+		padding: 0.5em 0.7em;
+		border: none;
+		border-radius: 0.5em;
+	}
+	#buttonAnimation {
+		bottom: 3;
+		transition: bottom 2s ease-in;
+	}
 }
 </style>
